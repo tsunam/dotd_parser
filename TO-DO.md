caafa8184464543e7bc010e1a445d9733fa04d84
GreenDragon <elf-github@arashi.com>
2015-02-25 22:55:14 -0800

    Rewired controller/views. Reporting back online. Now to learn CSS

---
+++ TO-DO.md
@@ -0,0 +1,22 @@
+* Add to parser/reports
+  * restorations ( health|honor )
+  * affected boss damage
+  * says ( personalized rants )
+  * applied ( magic )
+
+* Base / Proc damage break down
+  * Total Procs row
+    * Display avg proc damage/hit
+    * total proc damage
+    * % proc damage to total damage
+  * Base damage row
+    * Display avg base damage/hit
+    * total base damage
+    * % base damage to total damage
+  * Add % Base DMG column to Total procs/Inv Procs
+
+* Distinguish between on-hit drops ( You Found X! ) with proc-created drops ( You have obtained: x ). ( already done in obtained_items vs found_items )
+
+* LoTS support
+
+
--- controllers/default.py
+++ controllers/default.py
@@ -17,55 +17,86 @@ def index():
     if you need a simple wiki simply replace the two lines below with:
     return auth.wiki()
     """
-    response.flash = T("Welcome to web2py!")
-    return dict(message=T('Hello World'))
+    redirect(URL('form'))
+
+def form():
+    form = SQLFORM(db.raw_log)
+    form.vars.uuid = uuid_generator()
+
+    if form.process().accepted:
+        response.flash = T("Form Accepted")
+        redirect(URL('parsed', args=form.vars.uuid))
+    elif form.errors:
+        response.flash = T("Form has errors")
+    else:
+        response.flash = T("Please fill out the form")
+    return dict(form=form)
+
+def parsed():
+    if request.args:
+        uuid = request.args[0]
+        row = db(db.raw_log.uuid==uuid).select()
+        if len(row) == 0:
+            response.flash = T("UUID not found or purged from the DB")
+            redirect(URL('form'))
+        # We should never get here since uuid is a unique row in the table
+        if len(row) > 1:
+            response.flash = T("Something wicked this way went")
+            redirect(URL('form'))
+        # Leroy Jenkins! Let's do this!
+        experience, obtained_items, proc_items, found_items, log_file, max_hit, hit_list=parser(row[0].data)
+        return locals()
+    else:
+        response.flash = T("Expected UUID")
+        redirect(URL('form'))
 
 
-def user():
-    """
-    exposes:
-    http://..../[app]/default/user/login
-    http://..../[app]/default/user/logout
-    http://..../[app]/default/user/register
-    http://..../[app]/default/user/profile
-    http://..../[app]/default/user/retrieve_password
-    http://..../[app]/default/user/change_password
-    http://..../[app]/default/user/manage_users (requires membership in
-    use @auth.requires_login()
-        @auth.requires_membership('group name')
-        @auth.requires_permission('read','table name',record_id)
-    to decorate functions that need access control
-    """
-    return dict(form=auth())
+
+#def user():
+#    """
+#    exposes:
+#    http://..../[app]/default/user/login
+#    http://..../[app]/default/user/logout
+#    http://..../[app]/default/user/register
+#    http://..../[app]/default/user/profile
+#    http://..../[app]/default/user/retrieve_password
+#    http://..../[app]/default/user/change_password
+#    http://..../[app]/default/user/manage_users (requires membership in
+#    use @auth.requires_login()
+#        @auth.requires_membership('group name')
+#        @auth.requires_permission('read','table name',record_id)
+#    to decorate functions that need access control
+#    """
+#    return dict(form=auth())
 
 
-@cache.action()
-def download():
-    """
-    allows downloading of uploaded files
-    http://..../[app]/default/download/[filename]
-    """
-    return response.download(request, db)
+#@cache.action()
+#def download():
+#    """
+#    allows downloading of uploaded files
+#    http://..../[app]/default/download/[filename]
+#    """
+#    return response.download(request, db)
 
 
-def call():
-    """
-    exposes services. for example:
-    http://..../[app]/default/call/jsonrpc
-    decorate with @services.jsonrpc the functions to expose
-    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
-    """
-    return service()
+#def call():
+#    """
+#    exposes services. for example:
+#    http://..../[app]/default/call/jsonrpc
+#    decorate with @services.jsonrpc the functions to expose
+#    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+#    """
+#    return service()
 
 
-@auth.requires_login() 
-def api():
-    """
-    this is example of API with access control
-    WEB2PY provides Hypermedia API (Collection+JSON) Experimental
-    """
-    from gluon.contrib.hypermedia import Collection
-    rules = {
-        '<tablename>': {'GET':{},'POST':{},'PUT':{},'DELETE':{}},
-        }
-    return Collection(db).process(request,response,rules)
+#@auth.requires_login()
+#def api():
+#    """
+#    this is example of API with access control
+#    WEB2PY provides Hypermedia API (Collection+JSON) Experimental
+#    """
+#    from gluon.contrib.hypermedia import Collection
+#    rules = {
+#        '<tablename>': {'GET':{},'POST':{},'PUT':{},'DELETE':{}},
+#        }
+#    return Collection(db).process(request,response,rules)
