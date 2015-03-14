51285698337b856f2072002d07c2f69a0a88ec9f
GreenDragon <elf-github@arashi.com>
2015-03-13 22:55:39 -0700

    v1.4.1 Changes per GriffonSpade's Request

--- models/numbers.py
+++ models/numbers.py
@@ -6,7 +6,7 @@ def percentage(x, y):
     if y == 0:
         return "Divide/0 Error"
     else:
-        return commify( (float(x) / y) * 100)
+        return commify((float(x)/y) * 100)
 
 
 def commify(x):
@@ -25,11 +25,14 @@ def isnum(s):
         return True
 
 
-def safe_divide(x, y):
+def safe_divide(x, y, precision=2):
     if y == 0:
-        return "Divide/0 Error"
+        return "0"
     else:
-        return commify( (float(x) / y) )
+        if precision == 0:
+            return commify(int(round((float(x)/y), 0)))
+        else:
+            return commify(round((float(x)/y), 2))
 
 
 def gen_wiki_url(proc_name, suns_mode):
