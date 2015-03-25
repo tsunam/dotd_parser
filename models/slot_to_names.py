7b241391661decff325c070676d96573e6cb36f8
tsunam <tsunam@gmail.com>
2015-03-25 13:05:27 -0700

    Add equipType translation for gear that has a equipType, displaying as Slot in the procs table

---
+++ models/slot_to_names.py
@@ -0,0 +1,5 @@
+def load_slot_to_names():
+    #dawn slots: 1: Main Hand, 2: Off Hand, 3: helm, 4: chest, 5: gloves, 6: pants, 7: boots, 8: ring, 9: shield(offhand), 10: neck
+    #suns slots: 1: Main Hand, 2: Off Hand, 3: helm, 4: chest, 5: gloves, 6: pants, 7: boots, 8: ring
+    slots_dawn_translation = { "1":"Main Hand", "2":"Off Hand", "3":"Helm", "4":"Chest", "5":"Gloves", "6":"Pants", "7":"Boots", "8":"Ring", "9":"Off Hand", "10":"Neck"}
+    return slots_dawn_translation
